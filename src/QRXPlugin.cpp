2e45de2ab8c5cbbbf4ecd35da2cf22c71dfeddb9
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-03-14 23:39:22 +0100

    added CMakeSettings generator

--- src/QRXPlugin.cpp
+++ src/QRXPlugin.cpp
@@ -7,7 +7,7 @@ void init(rack::plugin::Plugin* p) {
    
    // Add modules here
    p->addModel(modelCVWizard);
-   
+
    // Any other plugin initialization may go here.
    // As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
 }
