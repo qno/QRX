e7fd0b4dbfeae0e62344b81a5660b06945d82456
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-03-16 21:58:25 +0100

    result of a training session with hippomocks

--- test/myCatch.cpp
+++ test/myCatch.cpp
@@ -5,6 +5,19 @@
 
 #include <string>
 
+#include <engine/Module.hpp>
+
+struct MyParam
+{
+   int x;
+   int y;
+};
+
+bool operator== (const MyParam& lhs, const MyParam& rhs)
+{
+   return (lhs.x == rhs.x) && (lhs.y == rhs.y);
+}
+
 class MockInterface
 {
 public:
@@ -12,7 +25,9 @@ public:
    virtual ~MockInterface() = default;
    virtual const int getInt666() = 0;
    virtual const std::string getStringMMM() = 0;
+   virtual const MyParam getParam(const MyParam& p) = 0;
 };
+
 
 TEST_CASE("Test1", "[simple]")
 {
@@ -27,4 +42,44 @@ TEST_CASE("Mockup", "[hippomock]")
    mockRepository.ExpectCall(mock, MockInterface::getStringMMM).Return(std::string{"MMM"});
    REQUIRE(666 == mock->getInt666());
    REQUIRE(std::string{"MMM"} == mock->getStringMMM());
+   
+   auto p = MyParam{1,2};
+   mockRepository.ExpectCall(mock, MockInterface::getParam).With(p).Return(p);
+   REQUIRE(mock->getParam(p) == p);
+}
+
+namespace rack {
+namespace engine {
+inline bool operator== (const Module::ProcessArgs& lhs, const Module::ProcessArgs& rhs)
+{
+   return (lhs.sampleRate == rhs.sampleRate) && (lhs.sampleTime == rhs.sampleTime);
+}
+}
+}
+
+TEST_CASE("ModuleMockup", "[rack]")
+{
+   using namespace rack::engine;
+   MockRepository mockRepository;
+   auto mock = mockRepository.Mock<Module>();
+   
+   auto args = Module::ProcessArgs{111.f, 222.f};
+   auto json = std::unique_ptr<json_t>(new json_t{JSON_INTEGER, 88});
+   
+   mockRepository.ExpectCall(mock, Module::process);
+   mockRepository.OnCall(mock, Module::onSampleRateChange);
+   mockRepository.OnCall(mock, Module::onReset);
+   mockRepository.OnCall(mock, Module::onRandomize);
+   mockRepository.OnCall(mock, Module::onAdd);
+   mockRepository.OnCall(mock, Module::onRemove);
+   mockRepository.ExpectCalls(mock, Module::dataToJson, 3).Return(json.get());
+   mockRepository.ExpectCalls(mock, Module::dataFromJson, 2);
+   
+   mock->process(args);
+   mock->onSampleRateChange();
+   REQUIRE(mock->dataToJson() == json.get());
+   REQUIRE(mock->dataToJson()->type == json->type);
+   REQUIRE(mock->dataToJson()->refcount == json->refcount);
+   mock->dataFromJson(json.get());
+   mock->dataFromJson(json.get());
 }
