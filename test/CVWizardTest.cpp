452d4217f9da42f5d8e6485e1d5a9304774fee18
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-03-22 10:42:00 +0100

    use SECTION in test

--- test/CVWizardTest.cpp
+++ test/CVWizardTest.cpp
@@ -8,29 +8,34 @@
 #include <catch2/catch.hpp>
 #include <fakeit.hpp>
 
+using namespace fakeit;
+using namespace qrx;
 
-TEST_CASE("dataToJson", "[cvwizard] [module]")
+TEST_CASE("CVWizardModule dataToJson", "[cvwizard] [module]")
 {
-   using namespace qrx;
-   
    CVWizardModule wiz;
-   auto expected = std::string{"{}"};
    auto jsonResult = std::unique_ptr<json_t>(wiz.dataToJson());
-   
    REQUIRE(jsonResult != nullptr);
-   REQUIRE(jsonResult->type == JSON_OBJECT);
    
-   const auto jsonParam1 = json_object_get(jsonResult.get(), "param1");
-   REQUIRE(jsonParam1 != nullptr);
-   REQUIRE(std::string(json_string_value(jsonParam1)) == std::string("0815"));
+   SECTION("json result type is JSON_OBJECT")
+   {
+      REQUIRE(jsonResult->type == JSON_OBJECT);
+   }
    
-   const auto jsonParam2 = json_object_get(jsonResult.get(), "param2");
-   REQUIRE(jsonParam2 != nullptr);
-   REQUIRE(json_integer_value(jsonParam2) == 2);
-   
-   const auto jsonParam3 = json_object_get(jsonResult.get(), "param3");
-   REQUIRE(jsonParam3 != nullptr);
-   REQUIRE(json_boolean_value(jsonParam3) == true);
+   SECTION("check json content")
+   {
+      const auto jsonParam1 = json_object_get(jsonResult.get(), "param1");
+      REQUIRE(jsonParam1 != nullptr);
+      REQUIRE(std::string(json_string_value(jsonParam1)) == std::string("0815"));
+      
+      const auto jsonParam2 = json_object_get(jsonResult.get(), "param2");
+      REQUIRE(jsonParam2 != nullptr);
+      REQUIRE(json_integer_value(jsonParam2) == 2);
+      
+      const auto jsonParam3 = json_object_get(jsonResult.get(), "param3");
+      REQUIRE(jsonParam3 != nullptr);
+      REQUIRE(json_boolean_value(jsonParam3) == true);
+   }
    
    //REQUIRE(json_dumps(jsonResult.get(), JSON_ENCODE_ANY) == "1");
 }
