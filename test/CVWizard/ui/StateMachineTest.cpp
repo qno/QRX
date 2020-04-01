0e14576c3eeaae580861032940f89e07a2d31f14
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-04-01 20:55:40 +0200

    created my first simply tinyfsm state machine

--- test/CVWizard/ui/StateMachineTest.cpp
+++ test/CVWizard/ui/StateMachineTest.cpp
@@ -5,7 +5,47 @@
 #include <CVWizard/ui/StateMachine.hpp>
 
 #include <catch2/catch.hpp>
-#include <fakeit.hpp>
+//#include <fakeit.hpp>
+
+#include <iostream>
+
+namespace x
+{
+// Events
+class MyToggleEvent : public tinyfsm::Event
+{
+public:
+   int x;
+};
+
+// Statemachine
+class MySM : public tinyfsm::Fsm<MySM>
+{
+public:
+   /* default reaction for unhandled events */
+   void react(tinyfsm::Event const &) { };
+   
+   virtual void react(MyToggleEvent const&);
+   
+   virtual void entry() { };  /* entry actions in some states */
+   void         exit()  { };  /* no exit actions */
+};
+// States
+class MyOffState;
+
+class MyOnState : public MySM
+{
+   void entry() override { std::cout << "* MyState is ON" << std::endl; };
+   void react(MyToggleEvent const &) override { transit<MyOffState>(); };
+};
+
+class MyOffState : public MySM
+{
+   void entry() override { std::cout << "* MyState is OFF" << std::endl; };
+   void react(MyToggleEvent const &) override { transit<MyOnState>(); };
+};
+}
+FSM_INITIAL_STATE(x::MySM, x::MyOffState);
 
 using namespace qrx::cvwizard::ui;
 
@@ -13,6 +53,18 @@ TEST_CASE("StateMachine", "[ui] [statemachine]")
 {
    auto sm = std::make_shared<StateMachine>();
    REQUIRE(sm);
+}
+
+TEST_CASE("TinyFSM playground", "[statemachine]")
+{
+   auto toggle = x::MyToggleEvent{};
+   
+   x::MySM::start();
+   
+   for (int i=0; i<7; ++i)
+   {
+      x::MySM::dispatch(toggle);
+   }
 }
 
 #pragma GCC diagnostic pop
