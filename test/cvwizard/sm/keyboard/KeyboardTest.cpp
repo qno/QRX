c411fdecc162dcd670ef4578f945c481f7cc7e90
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-04-05 10:35:51 +0200

    test for Keyboard state machine states

--- test/cvwizard/sm/keyboard/KeyboardTest.cpp
+++ test/cvwizard/sm/keyboard/KeyboardTest.cpp
@@ -13,9 +13,17 @@ FSM_INITIAL_STATE(Keyboard, Idle);
 TEST_CASE("Keyboard statemachine", "[keyboard] [statemachine]")
 {
    Keyboard::start();
+   REQUIRE(Keyboard::is_in_state<Idle>());
    Keyboard::dispatch(ControlKeyPressedEvent{});
+   REQUIRE(Keyboard::is_in_state<ControlKeyPressed>());
+   Keyboard::dispatch(ControlKeyReleasedEvent{});
+   REQUIRE(Keyboard::is_in_state<Idle>());
+   Keyboard::dispatch(ControlKeyPressedEvent{});
+   REQUIRE(Keyboard::is_in_state<ControlKeyPressed>());
    Keyboard::dispatch(MappingKeyEvent{});
+   REQUIRE(Keyboard::is_in_state<MappingModeActive>());
    Keyboard::dispatch(MappingCancelKeyEvent{});
+   REQUIRE(Keyboard::is_in_state<Idle>());
 }
 
 #pragma GCC diagnostic pop
