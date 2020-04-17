706d84248dfcb16dbc41a21ca877891c9fde5220
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-04-17 17:46:07 +0200

    add missed changes after refactoring commit

--- src/cvwizard/CVWizardRackModel.cpp
+++ src/cvwizard/CVWizardRackModel.cpp
@@ -1,12 +1,12 @@
 #include <cvwizard/CVWizardModule.hpp>
 #include <cvwizard/CVWizardWidget.hpp>
 #include <PluginSettings.hpp>
-#include <RackApp.hpp>
+
+#include <helpers.hpp>
 
 #include <memory>
 
 using namespace qrx::cvwizard;
 
-std::shared_ptr<qrx::boundary::RackAppBoundary> rackApp = std::make_shared<qrx::RackApp>();
 auto pluginSettings = std::make_shared<qrx::PluginSettings>();
-rack::Model* modelCVWizard = rack::createModel<CVWizardModule, CVWizardWidget>(CVWizardModule::SLUG);
+::rack::plugin::Model* modelCVWizard = ::rack::createModel<CVWizardModule, CVWizardWidget>(CVWizardModule::SLUG);
