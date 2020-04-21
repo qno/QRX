c11d67776912f17548886f791c42e11016071a09
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-04-21 20:58:34 +0200

    beginModuleMapping check if existing mapping exists

--- src/cvwizard/model/CVWizardModel.cpp
+++ src/cvwizard/model/CVWizardModel.cpp
@@ -13,13 +13,24 @@ CVWizardModel::~CVWizardModel()
 
 void CVWizardModel::beginModuleMapping(rack::ModuleWidget* widget)
 {
-   _currentModuleMapping = std::make_shared<ModuleMapping>(widget);
-   _moduleMappings.insert(std::make_pair(widget, _currentModuleMapping));
+   auto existingMapping = _moduleMappings.find(widget);
+   if (_moduleMappings.end() != existingMapping)
+   {
+      _currentModuleMapping = existingMapping->second;
+   }
+   else
+   {
+      _currentModuleMapping = std::make_shared<ModuleMapping>(widget);
+   }
+   _currentModuleMapping->enableHover();
 }
 
 void CVWizardModel::endModuleMapping()
 {
    _currentModuleMapping->disableHover();
+   // if param mappings has been added do
+   //_moduleMappings.insert(std::make_pair(widget, _currentModuleMapping));
+   _currentModuleMapping = nullptr;
 }
 
 std::shared_ptr<ModuleMapping> CVWizardModel::getCurrentModuleMapping() const
