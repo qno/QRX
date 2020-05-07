9f6a57f65c66e73553e4b494cb2db461eafbc6a9
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-05-07 14:03:01 +0200

    refactor alpha value animation into AnimatedValue abstract class

--- include/cvwizard/ui/OnHoverWidget.hpp
+++ include/cvwizard/ui/OnHoverWidget.hpp
@@ -1,25 +1,23 @@
 #pragma once
 
 #include <boundary/rack/Types.hpp>
-
-#include <chrono>
-#include <cmath>
+#include <cvwizard/ui/AnimatedValue.hpp>
 
 namespace qrx {
 namespace cvwizard {
 namespace ui {
 
-class HoveredWidget final : public boundary::rack::TransparentWidget
+class OnHoverWidget final : public boundary::rack::TransparentWidget, public AnimatedValue
 {
 public:
    
-   explicit HoveredWidget(boundary::rack::Widget* w)
+   explicit OnHoverWidget(boundary::rack::Widget* w)
       : _hoveredWidget{w}
    {
       box.size = _hoveredWidget->box.size;
    }
    
-   ~HoveredWidget() override
+   ~OnHoverWidget() override
    {
       if (_hoveredWidget)
       {
@@ -29,22 +27,7 @@ public:
    
    void step() override
    {
-      // https://stackoverflow.com/questions/14391327/how-to-get-duration-as-int-millis-and-float-seconds-from-chrono
-      // https://gafferongames.com/post/fix_your_timestep
-      const auto elapsed = std::chrono::steady_clock::now() - _start;
-      const auto seconds = std::chrono::duration_cast<std::chrono::duration<float>>(elapsed).count();
-   
-      const auto x = (seconds * AlphaSpeed);
-      const auto alpha = ((1.f + std::sin(x))/2.f * 255.f);
-      
-      if (alpha < 75.f)
-      {
-         _alpha = 75;
-      }
-      else
-      {
-         _alpha = static_cast<unsigned char>(alpha);
-      }
+      animate();
    }
    
    void draw(const boundary::rack::widget::DrawArgs& args) override
@@ -59,7 +42,7 @@ public:
          nvgCircle(vg, v, v, v + 3.f);
          nvgCircle(vg, v, v, v);
          nvgPathWinding(vg, NVG_HOLE);
-         nvgFillColor(vg, nvgRGBA(255, 0, 0, _alpha));
+         nvgFillColor(vg, nvgRGBA(255, 0, 0, _value));
          nvgFill(vg);
       }
       else
@@ -67,7 +50,7 @@ public:
          nvgLineCap(vg, NVG_ROUND);
          nvgLineJoin(vg, NVG_BEVEL);
          nvgStrokeWidth(vg, 5.f);
-         nvgStrokeColor(vg, nvgRGBA(255, 0, 0, _alpha));
+         nvgStrokeColor(vg, nvgRGBA(255, 0, 0, _value));
          nvgBeginPath(vg);
          nvgMoveTo(vg, 0, 0);
          nvgLineTo(vg, box.size.x, 0);
@@ -83,11 +66,7 @@ public:
 
 private:
    
-   static constexpr float AlphaSpeed = 2.f;
-   unsigned char _alpha = 0;
-   
    boundary::rack::Widget* _hoveredWidget = nullptr;
-   const std::chrono::time_point<std::chrono::steady_clock> _start = std::chrono::steady_clock::now();
 };
 
 }
