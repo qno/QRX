a14629d0a7ad701810a8809d834dfd8927da6636
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2021-07-22 23:34:29 +0200

    ignore subobject-linkage warning for SML only when gcc is compiler

--- thirdparty/sml/SML.cmake
+++ thirdparty/sml/SML.cmake
@@ -1,3 +1,5 @@
 add_library(SML INTERFACE)
-target_compile_options(SML INTERFACE -Wno-subobject-linkage)
+if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    target_compile_options(SML INTERFACE -Wno-subobject-linkage)
+endif ()
 target_include_directories(SML INTERFACE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/thirdparty/sml/include>)
